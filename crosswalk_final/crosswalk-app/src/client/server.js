c9b8e22563f35276e0f0290d8a5c7e8333bfd0ef
udoedo <udoedoudo@gmail.com>
2020-05-29 11:15:39 +0200

    updated services

--- crosswalk_final/crosswalk-app/src/client/server.js
+++ crosswalk_final/crosswalk-app/src/client/server.js
@@ -1,69 +1,78 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.get('/api/hello', (req, res) => {
-//   res.send({ express: 'Hello From Exfafasdfsadpress' });
-// });
-
-// app.post('/api/world', (req, res) => {
-//   console.log(req.body);
-//   res.send(
-//     `I received your POST request. This is what you sent me: ${req.body.post}`,
-//   );
-// });
-
-// app.listen(port, () => console.log(`Listening on port ${port}`));
-
-const express = require('express');
+const express = require("express");
 const cote = require('cote');
-const bodyParser = require('body-parser');
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-const notifyResponder = new cote.Responder({ 
+const http = require("http");
+const socketIo = require("socket.io");
+const bodyParser = require('body-parser');
+
+const port = 5000;
+const index = require("../routes/index");
+
+const app = express();
+app.use(index);
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+const io = socketIo(server);
+
+let interval;
+
+const notifyResponder = new cote.Responder({
   name: "notify user about distance to crosswalk - responder",
-  key:"notify"
+  key: "notify"
 });
 
-const clientLocationSender = new cote.Requester({ 
+const clientLocationSender = new cote.Requester({
   name: "sending pede/car location to monitor service - requester",
   key: 'sending_location'
 });
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-// send client to monitoring service
-app.post('/api/world', (req, res) => {
-  const request = {
-    type: 'send_client_info',
-    user: req.body,
-    crosswalks: req.body.near_crosswalks
+// initialize socket connection
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
   }
-  clientLocationSender.send(request, (err, res) => {
-    console.log("callback:",res);
+
+  // send client info to monitoring service
+  app.post('/api/world', (req, res) => {
+    console.log("post click")
+    // console.log(req)
+    const request = {
+      type: 'send_client_info',
+      user: req.body,
+      crosswalks: req.body.nearCrosswalks
+    }
+    // send back near crosswalks
+    clientLocationSender.send(request, (err, res) => {
+      socket.emit("nearCrosswalks", res);
+      // console.log("callback:", res);
+    });
+
+    res.send(req.body);
   });
 
-  res.send(req.body);
-})
+  // notify car if pedestrian is in crosswalk area and provide state of light
+  notifyResponder.on('notifUser', (req, cb) => {
+    const trafficLight = req.state_of_light;
+    const pedestrianInArea = req.pedestrian_in_area;
+    const crosswalkLocation = req.location;
 
-// notify car if pedestrian is in crosswalk area and provide state of light
-notifyResponder.on('notifUser', (req, cb) => {
-  const trafficLight = req.state_of_light;
-  const pedestrianInArea = req.pedestrian_in_area;
-  const crosswalkLocation = req.location
+    socket.emit("notifyInformation", {
+      pedestrianInArea: pedestrianInArea,
+      trafficLight: trafficLight,
+      crosswalkLocation: crosswalkLocation
+    });
+  });
 
-  console.log('Pedestrian in area: ', pedestrianInArea);
-  console.log('Traffic light: ', trafficLight);
-  console.log('Location: ', crosswalkLocation);
-  console.log('\n');
-})
 
-const port = 5000;
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
--- crosswalk_final/crosswalk-app/src/components/User.js
+++ crosswalk_final/crosswalk-app/src/components/User.js
@@ -1,12 +1,110 @@
-import React, {useState, useEffect} from 'react';
-import Header from "./layout/Header";
+// import React, {useState, useEffect} from 'react';
+// import Header from "./layout/Header";
+// import '../App.css';
+
+// function User() {
+//   const [clientID, setClientID] = useState('');
+//   const [clientInfo, setClientInfo] = useState('');
+//   const [clientLatitude, setClientLatitude] = useState('');
+//   const [clientLongitude, setClientLongitude] = useState('');
+
+//   const handleSubmit = async e => {
+//     e.preventDefault();
+//     const response = await fetch('/api/world', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({
+//         id: clientID,
+//         info: clientInfo,
+//         location: {
+//           latitude: parseFloat(clientLatitude),
+//           longitude: parseFloat(clientLongitude)
+//         } 
+//       }),
+//     });
+//   };
+
+//   return (
+    
+//     <div className="App">
+//         <Header/>
+//     <form onSubmit={handleSubmit}>
+//       <p>
+//         <strong>Client information:</strong>
+//       </p>
+//       <label>
+//         id:
+//         <input
+//           type="id"
+//           value={clientID}
+//           onChange={e => setClientID(e.target.value)}
+//         />
+//       </label>
+      
+//       <br></br>
+//       <label>
+//         info:
+//         <input
+//           type="info"
+//           value={clientInfo}
+//           onChange={e => setClientInfo(e.target.value)}
+//         />
+//       </label>
+//       <br></br>
+//       <label>
+//         latitude:
+//         <input
+//           type="latitude"
+//           value={clientLatitude}
+//           onChange={e => setClientLatitude(e.target.value)}
+//         />
+//       </label>
+//       <br></br>
+//       <label>
+//         longitude:
+//         <input
+//           type="id"
+//           value={clientLongitude}
+//           onChange={e => setClientLongitude(e.target.value)}
+//         />
+//       </label>
+//       <br></br>
+
+//       <button type="submit">Submit</button>
+//     </form>
+//   </div>
+//   );
+// }
+
+// export default User;
+
+
+import React, { useState, useEffect } from 'react';
+import socketIOClient from "socket.io-client";
 import '../App.css';
+
+const ENDPOINT = "http://127.0.0.1:5000";
 
 function User() {
   const [clientID, setClientID] = useState('');
   const [clientInfo, setClientInfo] = useState('');
   const [clientLatitude, setClientLatitude] = useState('');
   const [clientLongitude, setClientLongitude] = useState('');
+  const [nearCrosswalks, setNearCrosswalks] = useState();
+
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("notifyInformation", data => {
+      console.log(data);
+    });
+
+    socket.on("nearCrosswalks", data => {
+      setNearCrosswalks(data);
+    })
+  },[]);
 
   const handleSubmit = async e => {
     e.preventDefault();
@@ -21,61 +119,64 @@ function User() {
         location: {
           latitude: parseFloat(clientLatitude),
           longitude: parseFloat(clientLongitude)
-        } 
+        },
+        nearCrosswalks: nearCrosswalks
       }),
     });
   };
 
   return (
-    
     <div className="App">
-        <Header/>
-    <form onSubmit={handleSubmit}>
-      <p>
-        <strong>Client information:</strong>
-      </p>
-      <label>
-        id:
+      <form onSubmit={handleSubmit}>
+        <p>
+          <strong>Client information:</strong>
+        </p>
+        <p>
+          near crosswalks: {nearCrosswalks}
+        </p>
+        <label>
+          id:
         <input
-          type="id"
-          value={clientID}
-          onChange={e => setClientID(e.target.value)}
-        />
-      </label>
-      
-      <br></br>
-      <label>
-        info:
-        <input
-          type="info"
-          value={clientInfo}
-          onChange={e => setClientInfo(e.target.value)}
-        />
-      </label>
-      <br></br>
-      <label>
-        latitude:
-        <input
-          type="latitude"
-          value={clientLatitude}
-          onChange={e => setClientLatitude(e.target.value)}
-        />
-      </label>
-      <br></br>
-      <label>
-        longitude:
-        <input
-          type="id"
-          value={clientLongitude}
-          onChange={e => setClientLongitude(e.target.value)}
-        />
-      </label>
-      <br></br>
+            type="id"
+            value={clientID}
+            onChange={e => setClientID(e.target.value)}
+          />
+        </label>
 
-      <button type="submit">Submit</button>
-    </form>
-  </div>
+        <br></br>
+        <label>
+          info:
+        <input
+            type="info"
+            value={clientInfo}
+            onChange={e => setClientInfo(e.target.value)}
+          />
+        </label>
+        <br></br>
+        <label>
+          latitude:
+        <input
+            type="latitude"
+            value={clientLatitude}
+            onChange={e => setClientLatitude(e.target.value)}
+          />
+        </label>
+        <br></br>
+        <label>
+          longitude:
+        <input
+            type="id"
+            value={clientLongitude}
+            onChange={e => setClientLongitude(e.target.value)}
+          />
+        </label>
+        <br></br>
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
 
 export default User;
+
